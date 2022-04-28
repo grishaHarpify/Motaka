515a18e04fce6841cbdfa569eff71b89f81e4221
acagroup3 <nodejs.project.group3@gmail.com>
2022-04-28 23:22:22 +0400

    Fixed bugs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+require('dotenv').config()
 
 // For development 
 if (process.env.NODE_ENV === 'development') {
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,17 @@ const User = require('../models/User')
 const ConfirmCode = require('../models/ConfirmCode')
 const bcrypt = require('bcrypt')
 router.get('/create', async (req, res) => {
+  const { phone, password } = req.body
+
   await User.create({
-    phone: '+374 77 630905',
-    password: await bcrypt.hash('111111111', 12)
+    phone,
+    password: await bcrypt.hash(password, 12)
   })
-  res.send('Created!')
+
+  res.json({
+    status: 'User created!',
+    phone
+  })
 })
 router.get('/test', async (req, res) => {
   const test = await ConfirmCode.findOne({ userId: '626a74660d035090a352dbb9' }).populate('userId')
