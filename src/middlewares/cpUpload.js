68d5cb76b10c18943c993846a0390b976901fc5f
acagroup3 <nodejs.project.group3@gmail.com>
2022-05-05 17:43:32 +0400

    Deleted unused

--- src/middlewares/cpUpload.js
+++
@@ -1,6 +0,0 @@
-// To get data from [form-data] (files and images in req.files)
-const multer = require('multer')
-const upload = multer(({ storage: multer.memoryStorage() }))
-const cpUpload = upload.fields([{ name: 'photo', maxCount: 15 }, { name: 'pdfFile', maxCount: 15 }])
-
-module.exports = cpUpload
