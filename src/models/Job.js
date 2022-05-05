923f66e7de69de7dcfe758a1f9c9f6efcb9f265c
acagroup3 <nodejs.project.group3@gmail.com>
2022-05-05 19:33:28 +0400

    changed job model

--- src/models/Job.js
+++ src/models/Job.js
@@ -1,14 +1,16 @@
 const { Schema, model } = require('mongoose')
 
 const jobSchema = new Schema({
-  startDate: { type: Date },
+  startDate: { type: Date }, // date and time
   duration: { type: String },
-  money: { type: String },
+  cost: { type: String },
   address: { type: String },
   category: {
     type: String,
+    // karanq sarqenq poqr root vor vra fronty request ani uxarki category-n stana subCategories
     enum: ['cleaning', 'repairing', 'plumbing', 'petWalking', 'ironing']
-  }
+  },
+  subCategories: [{ type: String }]
 }, {
   collection: 'jobs', strict: false
 })
