3a7743adec0633cc0a25b1c7a370883a1a6868fc
acagroup3 <nodejs.project.group3@gmail.com>
2022-05-14 15:37:44 +0400

    Added query object filter for getAllJobs

---
+++ src/middlewares/filterRequestQuery.js
@@ -0,0 +1,28 @@
+
+
+function filterGetAllJobsQuery(req, res, next) {
+  const queryObject = req.query
+
+  for (let key in queryObject) {
+    // if key is not in case => delete it
+    switch (key) {
+      case 'salcost': // salary.cost
+      case 'caties':  // categories
+      case 'date':    // startDate
+      case 'dur':     // duration
+      case 'page':    // pagination and limit
+      case 'limit':
+        break
+      default:
+        delete queryObject[key]
+    }
+  }
+
+
+  next()
+}
+
+
+module.exports = {
+  filterGetAllJobsQuery
+}
