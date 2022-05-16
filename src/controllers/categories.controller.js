ba8b651e3afcc83e2c3bc26acce4954b76132284
acagroup3 <nodejs.project.group3@gmail.com>
2022-05-16 15:52:28 +0400

    Added 2 rutes

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -1,11 +1,51 @@
+const CategoryModel = require('../models/Category')
+
 
 async function getAllCategories(req, res) {
-  res.send('ok')
+  try {
+    // get all categories
+    const allCategories = await CategoryModel.find()
+
+    res.json({
+      count: allCategories.length,
+      data: allCategories
+    })
+  } catch (e) {
+    console.log(`Error in file: ${__filename}!`)
+    console.log(e.message)
+    res.status(500).json({
+      errorType: 'Server side error!',
+      errorMsg: e.message,
+    })
+  }
 }
 
 
 async function getCategoryDataWithId(req, res) {
-  res.send(`ok ${req.params.categoryId}`)
+  try {
+    const { categoryId } = req.params
+
+    const categoryData = await CategoryModel.findById(categoryId)
+
+    // Check category with such id exist or not
+    if (!categoryData) {
+      return res.status(400).json({
+        errorType: 'Incorrect ID error!',
+        errorMsg: 'Category with such ID does not exist.',
+      })
+    }
+
+    res.json({
+      categoryData
+    })
+  } catch (e) {
+    console.log(`Error in file: ${__filename}!`)
+    console.log(e.message)
+    res.status(500).json({
+      errorType: 'Server side error!',
+      errorMsg: e.message,
+    })
+  }
 }
 
 module.exports = {
--- src/routes/categories.router.js
+++ src/routes/categories.router.js
@@ -14,6 +14,6 @@ const idValidation = require('../middlewares/IDValidation')
 
 // Routes //
 categoriesRouter.get('/', categoriesController.getAllCategories)
-categoriesRouter.get('/:categoryId/:id', idValidation, categoriesController.getCategoryDataWithId)
+categoriesRouter.get('/:categoryId', idValidation, categoriesController.getCategoryDataWithId)
 
 module.exports = categoriesRouter
