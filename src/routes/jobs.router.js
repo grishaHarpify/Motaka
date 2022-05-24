0831d771ee88aee54e5ab8335e3c205fbfff5159
acagroup3 <nodejs.project.group3@gmail.com>
2022-05-24 15:57:37 +0400

    Added comments

--- src/routes/jobs.router.js
+++ src/routes/jobs.router.js
@@ -62,7 +62,7 @@ jobsRouter.patch('/:jobId', pathIdValidation, jobsController.editJobWithId)
 // Add new applicant to job
 jobsRouter.patch('/:jobId/candidate', isProvider, pathIdValidation, jobsController.addJobNewCandidate)
 
-
+// Cancel job
 jobsRouter.delete('/:jobId', isUser, pathIdValidation, jobsController.cancelJob)
 
 module.exports = jobsRouter
