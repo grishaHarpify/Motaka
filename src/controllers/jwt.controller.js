c1678c22174026b20b7327ff644e42c3fe01d467
acagroup3 <nodejs.project.group3@gmail.com>
2022-05-27 17:38:52 +0400

    Added swagger and refresh token validation

--- src/controllers/jwt.controller.js
+++ src/controllers/jwt.controller.js
@@ -1,22 +1,63 @@
-
 const jwt = require('jsonwebtoken')
+
+const UserModel = require('../models/User')
+
 const JWTHandler = require('../services/jwtHandler')
 
-
-function getRefreshReturnPair(req, res) {
+async function getRefreshReturnPair(req, res) {
   try {
     const { refreshToken } = req.body
 
-    // Get user id from jwt
-    const { userId } = jwt.verify(refreshToken, process.env.JWT_SECRET)
 
-    // Create new access and refresh tokens
-    const accessToken = JWTHandler.createAccessToken(userId)
-    const newRefreshToken = JWTHandler.createRefreshToken(userId)
+    let userInfo
+    try {
+      userInfo = jwt.verify(refreshToken, process.env.JWT_SECRET)
+    } catch (e) {
+      if (e.name === 'TokenExpiredError') {
+        // Token expired
+        return res.status(401).json({
+          errorType: 'JWT error!',
+          errorMessage: 'Refresh token date is expired.'
+        })
+      }
 
-    res.json({
-      accessToken,
-      refreshToken: newRefreshToken
+      // Token invalid
+      return res.status(401).json({
+        errorType: 'JWT error!',
+        errorMessage: 'Refresh token is not valid.'
+      })
+    }
+
+    // Get userId from token 
+    const { userId } = userInfo
+    // Check (Argument passed in must be a string of 12 bytes or a string of 24 hex characters)
+    if (userId.length === 12 || userId.length === 24) {
+      // Id correct
+      // Get user data from DB
+      const userFromDb = await UserModel.findOne({ _id: userId })
+
+      if (!userFromDb) {
+        // in JWT incorrect ID
+        return res.status(401).json({
+          errorType: 'JWT error!',
+          errorMessage: 'Wrong refresh token.'
+        })
+      }
+
+      // Create new access and refresh tokens
+      const accessToken = JWTHandler.createAccessToken(userId)
+      const newRefreshToken = JWTHandler.createRefreshToken(userId)
+
+      return res.json({
+        accessToken,
+        refreshToken: newRefreshToken
+      })
+    }
+
+    // User id in token incorrect
+    res.status(401).json({
+      errorType: 'JWT error!',
+      errorMessage: 'Wrong authorization token.'
     })
 
   } catch (e) {
--- src/routes/jwt.router.js
+++ src/routes/jwt.router.js
@@ -9,3 +9,98 @@ jwtRouter.post('/refresh',
   jwtController.getRefreshReturnPair)
 
 module.exports = jwtRouter
+
+/**
+ * @swagger
+ * components:
+ *  tags:
+ *   - name: JWT
+ *  securitySchemes:
+ *     access-token:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *  schemas:
+ *   test:
+ *    type: object
+ *    properties:
+ *      next:
+ *        type: object
+ *        properties:
+ *          page:
+ *            type: number
+ *            example: 3
+ *          limit:
+ *            type: number
+ *            example: 15
+ */
+
+/**
+ * @swagger
+ * /jwt/refresh:
+ *  post:
+ *   tags: [JWT]
+ *   description: Send refresh token and get new access and refresh tokens
+ *	  requestBody:
+ *	   description: Refresh token
+ *	   required: true
+ *	   content:
+ *	     application/json:
+ *       schema:
+ *         properties:
+ *           refreshToken:
+ *             type: string
+ *             example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXCJ9.eyJ1c2VySWQiOiI2MjkY2E2MWU4MTEzNmY1YjU3OTcyOGYiLCJpYXQiOjE2NTM2NTcxMjYsImV4cCI6MTY1NDI2MTkyNn0.SgGPtXMCc68oU1oeuBJVyLK-4DPCqfEZJbX_IM5T-g 
+ *
+ *   responses:
+ *     201:
+ *	     description: New access and refresh tokens
+ *	     content:
+ *	       application/json:
+ *         schema:
+ *           properties:
+ *             accessToken:
+ *              type: string
+ *              example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX9.eyJ1c2VySWQiOiI2Y2E2MWU4MTEzNmY1YjU3OTcyOGYiLCJpYXQiOjE2NTM2NTc0OTEsImV4cCI6MTY1MzY3NTQ5MX0.0iT8aLZV-dG3cL673S3GyroKYH3uDQaU-6NZtKG
+ *             refreshToken:
+ *              type: string
+ *              example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXV9.eyJ1c2VySWQiOiI2MkwY2E2MWU4MTEzNmY1YjU3OTcyOGYiLCJpYXQiOjE2NTM2NTc0OTEsImV4cCI6MTY1NDI2MjI5MX0.knt7zINga4Z3QxNl0KdrTyBaOSI6Udf-s9HCE6s
+ *     400:
+ *	     description: Bad request
+ *	     content:
+ *	       application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/JobValidationSchema'
+ *     401:
+ *	     description: Unauthorized
+ *	     content:
+ *	       application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/VerifyJwtSchema'
+ *     403:
+ *	     description: Forbidden
+ *	     content:
+ *	       application/json:
+ *         schema:
+ *           properties:
+ *             errorType:
+ *              type: string
+ *              example: Forbidden!
+ *             errorMessage:
+ *              type: string
+ *              example: Only users with an active role [user] have access in this route.
+ *
+ *     500:
+ *	     description: Server side error
+ *	     content:
+ *	       application/json:
+ *         schema:
+ *           properties:
+ *             errorType:
+ *              type: string
+ *              example: Server side error!
+ *             errorMessage:
+ *              type: string
+ *              example: ...
+ *
+ */
